23a64985f432b1992ff085d9cb114721a45b0d9a
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-11-27 11:52:33 -0300

    Terminando os testes para a ListaDuplamenteEncadeada

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import one.digitalinnovation.ListaDuplamenteEncadeada.ListaDuplamenteEncadeada;
 import one.digitalinnovation.ListaEncadeada.ListaEncadeada;
 
 public class Main {
     public static void main(String[] args) {
+        // caso queira testar cada classe, remova os simbolos de comentado no codigo abaixo :
                              /* TESTE PARA A ListaEncadeada */
+        /*
         ListaEncadeada<String> ListaEncadeada = new ListaEncadeada<>();
 
         ListaEncadeada.add("teste1");
@@ -24,5 +27,32 @@ public class Main {
         System.out.println(ListaEncadeada.remove(3));
 
         System.out.println(ListaEncadeada);
+        */
+
+                           /* TESTE PARA A ListaDuplamenteEncadeada */
+
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada<>();
+
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+
+        System.out.println(" ");
+        System.out.println(minhaListaEncadeada);
+        System.out.println(" ");
+
+        minhaListaEncadeada.remove(3);
+        minhaListaEncadeada.add(3,"P2");
+
+        System.out.println("Removendo a string c3 no indice 3 e colocando outra string no lugar:");
+        System.out.println(minhaListaEncadeada);
+        System.out.println(" ");
+        System.out.println("Imprimindo o elemento de indice 3: ");
+        System.out.println(minhaListaEncadeada.get(3));
+
     }
 }

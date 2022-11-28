eddb42271d9c52b333b27fe08e34dbf108707c10
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-11-28 11:03:09 -0300

    Criacao do metodo getNo

--- src/main/java/one/digitalinnovation/ListaCircular/ListaCircular.java
+++ src/main/java/one/digitalinnovation/ListaCircular/ListaCircular.java
@@ -3,8 +3,26 @@ package one.digitalinnovation.ListaCircular;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+
+
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+
+        if(index == 0) {
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
 
     public boolean isEmpty() {
         return this.tamanhoLista == 0 ? true : false;
